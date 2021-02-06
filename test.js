18145fb39e69696cbe1e4d357970887e31686eea
PSarapata <pspsarapata@gmail.com>
2021-02-06 22:51:50 +0100

    Small typo corrections.

--- test.js
+++ test.js
@@ -4,12 +4,13 @@ import { Selector } from 'testcafe';
 // TestCafe hook to slow down each test, so that you can observer what's going on. Don't use it in production environment.
 fixture `Working...`
     .page `https://secure.axainsurance.com/Sales/PersonalLines/AxaDirect/Axa/Travel/Standard/Desktop/Quote/Entry`
+    // UNCOMMENT LINES BELOW IF YOU FEEL DIZZY LOOKING AT THE TEST PREVIEWS:
     // .beforeEach(async t => {
     //     await t.setTestSpeed(0.5);
     // })
 // prettier-ignore
 test('Testing Axa Travel', async t => {
-    // Navigating the DOM tree in search of find targets...
+    // Navigating the DOM tree in search of target elements...
     // first_page
     const single_trip_button = Selector('#select-single-trip');
     const today_option = Selector('#departureDate > div > div > div > div > button');
